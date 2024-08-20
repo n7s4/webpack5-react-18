dcde919d281d97fc7087cd0a56887b061650bbf0
ns <1464521427@qq.com>
2024-08-20 22:31:12 +0800

    3th

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 import "./app.css";
 import ThemeContext from "./components/context/ThemeContext";
+import UseFefs from "@/components/context/useRefs"
 const App = () => {
-  return <ThemeContext />;
+  // return <ThemeContext />;
+  return <UseFefs></UseFefs>
 };
+
+
+// 发布订阅模型
+class DataSource {
+  lintener: any[];
+  constructor() {
+    // 订阅者
+    this.lintener = []
+  }
+  // 订阅
+  addChangeListener(callback: (data: string) => void) {
+    this.lintener.push(callback)
+  }
+  // 发布
+  publish(data: any) {
+    this.lintener.forEach(item => item(data))
+  }
+}
+const data_source = new DataSource()
+data_source.addChangeListener((data) => {
+  console.log('订阅者1', data)
+})
+data_source.addChangeListener((data) => {
+  console.log('订阅者2', data)
+})
+data_source.publish('nba')
 export default App;
