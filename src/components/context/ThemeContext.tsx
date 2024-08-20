ba10b91683d145c8e9a05bae771693308325d4c5
wen xin <wenxin@thinkerx.com>
2024-08-20 16:03:56 +0800

    context消费者可以更新组件而不是state

--- src/components/context/ThemeContext.tsx
+++ src/components/context/ThemeContext.tsx
@@ -3,6 +3,7 @@ const ThemeContext = createContext<any | null>(null);
 
 export default function ThemeApp() {
   const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(0);
   const handleClick = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
@@ -11,28 +12,51 @@ export default function ThemeApp() {
       value={{
         theme,
         setTheme,
+        count,
       }}
     >
       <Form />
+      <button onClick={() => setCount(count + 1)}>增加</button>
       {/* <button onClick={() => handleClick()}>change</button> */}
     </ThemeContext.Provider>
   );
 }
-const Form: React.FC = (): JSX.Element => {
-  return (
-    <>
-      <h1>WelCome</h1>
-      <Theme />
-    </>
-  );
-};
+// const Form: React.FC = () => {
+//   return (
+//     <>
+//       <h1>WelCome</h1>
+//       <Theme />
+//     </>
+//   );
+// };
+class Form extends React.Component {
+  shouldComponentUpdate(
+    nextProps: Readonly<{}>,
+    nextState: Readonly<{}>,
+    nextContext: any
+  ): boolean {
+    return false;
+  }
+  render(): React.ReactNode {
+    console.log("render Form");
+
+    return (
+      <>
+        <h1>WelCome</h1>
+        <Theme />
+      </>
+    );
+  }
+}
+
 const Theme: React.FC = () => {
-  const { theme, setTheme } = useContext(ThemeContext);
+  const { theme, setTheme, count } = useContext(ThemeContext);
   const handleClick = () => {
     setTheme(theme === "light" ? "dark" : "light");
   };
   return (
     <div>
+      <h2>{count}</h2>
       <h2>{theme}</h2>
       <button onClick={() => handleClick()}>change</button>
     </div>
